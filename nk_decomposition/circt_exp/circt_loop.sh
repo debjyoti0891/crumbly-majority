8c59a5172ede0ac624f9f73c49d814a1fda7ee5a
Debjyoti Bhattacharjee <bhatta53@imec.be>
2024-10-21 10:05:29 +0200

    [feat] multiple changes to mlir test for maj

--- nk_decomposition/circt_exp/circt_loop.sh
+++ nk_decomposition/circt_exp/circt_loop.sh
@@ -5,14 +5,15 @@ output_json=tcad.json
 rm out.log
 ARTEFACTS_DIR=artefacts
 mkdir -p ${ARTEFACTS_DIR}
-
-for (( n=5; n<=512; n+=2 ))
+mv tcad.json "tcad.json.b$(date +%d%m%y%H%M)"
+RES="res_$(date +%d%m%y%H%M)"
+for (( n=1001; n<=1002; n+=2 ))
 do
   # Generate the MLIR file with the appropriate structure using the Python script
       python3 gen_mlir_circt.py $n
 
   # # Iterate over k from n-5 to n in odd steps
-  for k in 5 7 9 11 21 51 101
+  for k in 5 7 9 13 17 21 51 101
   do
   #   # If k is less than 5, do nothing
     if [ $k -gt $n ]; then
@@ -20,28 +21,39 @@ do
         continue
     fi
       # k=9
-      RES="res_${k}"
+
       echo $n "->" $k "synthesis in progress" | tee -a out.log
       # Run the toyc-ch7 command on the generated MLIR file
       # $MLIR_BIN -emit=mlir-synth maj$n.mlir 2> maj${n}_${k}.out | tee -a out.log
-      $MLIR_BIN --maj-to-counter="counter-input-count=$k" --counter-to-maj maj_$n.mlir > maj${n}_${k}.mlir
-      $MLIR_BIN --view-op-graph maj${n}_${k}.mlir > o 2> maj${n}_${k}.dot
+      c=$(((k + 1) / 2))
+      $MLIR_BIN --maj-to-counter="counter-input-count=$c" --counter-to-maj maj_$n.mlir > maj${n}_${k}.mlir
+      $MLIR_BIN --hw-dce maj${n}_${k}.mlir > maj${n}_${k}_dce.mlir
+      # python3 trim_trailing_ops.py maj${n}_${k}.mlir
+      $MLIR_BIN --view-op-graph maj${n}_${k}_dce.mlir > o 2> maj${n}_${k}.dot
       grep '^\s*[a-zA-Z0-9_]\+\s*\[' maj${n}_${k}.dot | grep sls > nodes
       python3 py_node_count.py nodes $n $k $output_json
   #   fi
 
 
-      # python3 mlir2c.py maj${n}_${k}.out
-      # g++ hack${n}_${k}.cpp -o hack${n}_${k}
-      # ./hack${n}_${k} > verifhack${n}_${k}
-      # echo -n verifhack${n}_${k} " " >> $RES
-      # grep PASS verifhack${n}_${k} >> $RES
-      # grep error verifhack${n}_${k} >> $RES
+      python3 ../mlir2c.py maj${n}_${k}_dce.mlir
+      g++ hack${n}_${k}_dce.cpp -o hack${n}_${k}
+      ./hack${n}_${k} > verifhack${n}_${k}
+      echo -n verifhack${n}_${k} " " >> $RES
+      grep PASS verifhack${n}_${k} >> $RES
+      grep error verifhack${n}_${k} >> $RES
+
       mv maj${n}_${k}.mlir ${ARTEFACTS_DIR}
+      mv maj${n}_${k}_dce.mlir ${ARTEFACTS_DIR}
       mv maj${n}_${k}.dot ${ARTEFACTS_DIR}
+      mv hack${n}_${k}_dce.cpp ${ARTEFACTS_DIR}
+      mv hack${n}_${k} ${ARTEFACTS_DIR}
+      mv verifhack${n}_${k} ${ARTEFACTS_DIR}
+      # mv maj${n}_${k}_trimmed.mlir ${ARTEFACTS_DIR}
+
     # fi
   done # k
   mv maj_$n.mlir ${ARTEFACTS_DIR}
 done
 \rm o nodes
+echo "verification output: " $RES
 # python3 parse_all_stats.py out.log $k
--- nk_decomposition/circt_exp/plot_figs.py
+++ nk_decomposition/circt_exp/plot_figs.py
@@ -3,6 +3,25 @@ import plotly.express as px
 import glob
 import json
 import sys
+import math
+import plotly.graph_objects as go
+yaxes_offset = 1000
+def gate_bound_asymptotic(n, k):
+    l =  math.ceil(k/2)
+    bound =   n * (math.log2(k)*math.log2(k)) / k
+    return int(math.ceil(bound))
+
+def upper_gate_bound(n, k):
+    l =  math.ceil(k/2)
+    p = int(math.log2(l))
+    bound = 2 *( (2*n + k) * (math.log2(k)**2) + 2*n*(k+1))*(k + math.log2(k))
+    # bound = 2 *((2*n + 2*k + k*p + 1)*p + n*(k+1))*(2*l+p)
+    bound = bound / (k*(k+1))
+    return int(math.ceil(bound))
+
+def lower_gate_bound(n, k):
+    bound = n*math.log(n)/k
+    return int(math.ceil(bound))
 
 # Function to create a stacked bar chart for a given k
 def stacked_bar_chart_k(df, k, output_dir='.', n=None):
@@ -21,12 +40,14 @@ def stacked_bar_chart_k(df, k, output_dir='.', n=None):
         template="simple_white"
         # category_orders={'n': sorted(df_k['n'].unique())}
     )
-    fig.update_traces(texttemplate="%{y}")
+    # fig.update_traces(texttemplate="%{y}")
     fig.update_xaxes(type='category')
+    fig.update_layout(yaxis=dict(range=[0, max(df_k['bound'].max(), df_k['sum'].max()) + yaxes_offset]))
+
     fig.update_layout(
     legend=dict(
         x=.1,
-        y=.9,
+        y=.95,
         traceorder="normal",
         font=dict(
             family="sans-serif",
@@ -35,6 +56,39 @@ def stacked_bar_chart_k(df, k, output_dir='.', n=None):
         ),
     )
     )
+    # Create the line chart trace with markers only, large size, and value labels
+    line_trace = go.Scatter(
+        x=df_k['n'],
+        y=df_k['bound'],
+        mode='markers+text',  # Ensure markers and text are both plotted
+        name='Upper Bound',
+        marker=dict(
+            color='mediumseagreen',
+            size=12,  # Large marker size
+            symbol='x'  # Marker symbol 'x'
+        ),
+        text=df_k['bound'],  # Display values of the markers
+        textposition='top center'  # Position of the text relative to markers
+    )
+    # Add the line chart trace to the figure
+    fig.add_trace(line_trace)
+
+    line_trace = go.Scatter(
+        x=df_k['n'],
+        y=df_k['lbound'],
+        mode='markers+text',  # Ensure markers and text are both plotted
+        name='Lower Bound',
+        marker=dict(
+            color='firebrick',
+            size=12,  # Large marker size
+            symbol='hexagon2-dot'  # Marker symbol 'x'
+        ),
+        text=df_k['lbound'],  # Display values of the markers
+        textposition='top center'  # Position of the text relative to markers
+    )
+    # Add the line chart trace to the figure
+    fig.add_trace(line_trace)
+
 
     fig.write_image(f"{output_dir}/stacked_bar_chart_k_{k}.pdf")
     fig.show()
@@ -57,11 +111,14 @@ def stacked_bar_chart_n(df, n, output_dir='.', k=None):
         template="simple_white"
         # category_orders={'n': sorted(df_k['n'].unique())}
     )
-    fig.update_traces(texttemplate="%{y}")
+    fig.update_xaxes(type='category')
+
+    fig.update_layout(yaxis=dict(range=[0, max(df_k['bound'].max(), df_k['sum'].max()) + yaxes_offset]))
+    # fig.update_traces(texttemplate="%{y}")
     fig.update_layout(
     legend=dict(
-        x=.7,
-        y=.9,
+        x=.8,
+        y=.95,
         traceorder="normal",
         font=dict(
             family="sans-serif",
@@ -70,24 +127,68 @@ def stacked_bar_chart_n(df, n, output_dir='.', k=None):
         ),
     )
     )
-    fig.update_xaxes(type='category')
+
+    # Create the line chart trace with markers only, large size, and value labels
+    line_trace = go.Scatter(
+        x=df_k['k'],
+        y=df_k['bound'],
+        mode='markers+text',  # Ensure markers and text are both plotted
+        name='Upper Bound',
+        marker=dict(
+            color='mediumseagreen',
+            size=12,  # Large marker size
+            symbol='x'  # Marker symbol 'x'
+        ),
+        text=df_k['bound'],  # Display values of the markers
+        textposition='top center'  # Position of the text relative to markers
+    )
+    # Add the line chart trace to the figure
+    fig.add_trace(line_trace)
+    # Create the line chart trace with markers only, large size, and value labels
+    line_trace = go.Scatter(
+        x=df_k['k'],
+        y=df_k['lbound'],
+        mode='markers+text',  # Ensure markers and text are both plotted
+        name='Lower Bound',
+        marker=dict(
+            color='firebrick',
+            size=12,  # Large marker size
+            symbol='hexagon2-dot'  # Marker symbol 'x'
+        ),
+        text=df_k['lbound'],  # Display values of the markers
+        textposition='top center'  # Position of the text relative to markers
+    )
+    # Add the line chart trace to the figure
+    fig.add_trace(line_trace)
     fig.write_image(f"{output_dir}/stacked_bar_chart_k_{k}.pdf")
     fig.show()
 
 # Function to create a line plot for each k with sum of logicsynthesis columns
 def line_plot_sum_k(df, output_dir='.'):
-    df = df[df['k'] != 7]
-    df['sum'] = df[['and', 'or', 'maj', 'not']].sum(axis=1)
+    df = df[df['k'].isin({5,9,17,51,101})]
     fig = px.line(
-        df, x='n', y='sum', color='k',
+        df, x='n', y='sum',color='k',
         # title='Line Plot of Sum of Logic Synthesis Values for each k',
         labels={'sum': '#Gates', 'n': 'n'},
         markers=True,
         symbol=df['k'],
         template="simple_white"
     )
+
+    # # Melt the DataFrame to long format for Plotly Express
+    # df_melted = df.melt(id_vars=['n', 'k'], value_vars=['sum'],
+    #                     var_name='Metric', value_name='Values')
+
+    # # Create a plot using Plotly Express
+    # fig = px.line(df_melted, x='n', y='Values', color='k', line_dash='Metric',
+    #             labels={'Values': 'Values', 'Metric': 'Metric'},
+    #             title='Gates and Bound vs n for each k',
+    #                     template="simple_white"
+    #     )
+
     fig.update_traces(mode='lines', marker_line_width=2, marker_size=20)
     fig.update_xaxes(type='category')
+    fig.update_layout(yaxis=dict(range=[1, max(df['bound'].max(), df['sum'].max())]))
     fig.update_layout(
     legend=dict(
         x=.1,
@@ -102,6 +203,7 @@ def line_plot_sum_k(df, output_dir='.'):
     )
     fig.write_image(f"{output_dir}/line_plot_sum_k.pdf")
     fig.show()
+
 
 def dict_to_dataframe(data):
   """
@@ -133,6 +235,7 @@ if __name__ == "__main__":
 
     print(data[0])
     df = dict_to_dataframe(data)
+    # df = df[pd.to_datetime(df['timestamp']).dt.date == pd.Timestamp.today().date()]
     df.to_csv('test.csv')
     # df = pd.concat(df_list, ignore_index=True)
 
@@ -141,15 +244,26 @@ if __name__ == "__main__":
         'sls.and': 'and',
         'sls.or': 'or',
         'sls.maj': 'maj',
-        'sls.not': 'not'
+        'sls.not': 'not',
     }, inplace=True)
-
+    # df['k'] = 2*df['c'] - 1
     # Example usage
+    df['bound'] = df.apply(lambda row: upper_gate_bound(row['n'], row['k']), axis=1)
+    df['lbound'] = df.apply(lambda row: lower_gate_bound(row['n'], row['k']), axis=1)
+
+    df['sum'] = df[['and', 'or', 'maj', 'not']].sum(axis=1)
+    df.to_csv('raw_data.csv')
+
     k_value = 9
     n_value = 201
 
+    # print(list(df['k'].unique()))
+    # print(list(df['n'].unique()))
+
     output_dir = '.'  # Define your output directory here if needed
 
-    stacked_bar_chart_k(df, k_value, output_dir, [21, 51, 101, 201, 501])
-    stacked_bar_chart_n(df, n_value, output_dir, [5,9,11,21,51])
-    line_plot_sum_k(df, output_dir)
+    # stacked_bar_chart_k(df, k_value, output_dir, [17,51,101,201,511])
+    # n_value = 511
+    n_value = 1001
+    stacked_bar_chart_n(df, n_value, output_dir, [5,9,17,51,101])
+    # line_plot_sum_k(df, output_dir)
